069e635c21ac36775d9bc8ed2cd09c41d7ba965f
Ardi Jorganxhi <ardijorganxhi@eskisehir.edu.tr>
2022-10-30 23:25:55 +0300

    Login and register parts are coded

---
+++ EstuSozluk.API/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace EstuSozluk.API.Models
+{
+    public class User
+    {
+       public string username { get; set; }
+       public string email { get; set; }
+       public string password { get; set; }
+       public string nickname { get; set; }
+       public int roleId { get; set; }
+    }
+}
